3211d914dc73b130a730f2a8105501e5c7cceb3e
Nitincodes02 <165637495+Nitincodes02@users.noreply.github.com>
2024-04-10 13:38:23 +0530

    Add files via upload

---
+++ Cult-fit-clone/scripts/index.js
@@ -0,0 +1,14 @@
+import { navbar } from "../components/navbar.js";
+import { footer } from "../components/footer.js";
+
+document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
+
+document.getElementById("icon").addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+
+document.getElementById("userPic").addEventListener("click", () => {
+    window.location.href = "profile.html";
+});
+
